4275c5d8d7809bd9599e5d9f89662010f95a62b5
GiorgioDordo <djole6789@gmail.com>
2024-11-12 12:49:27 +0100

    created rooute index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MangaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/mangas', [MangaController::class, 'index'])->name('mangas');

7f064c8c5875caa1b39ede9489ad41b2906fead9
Marcap00 <cappellatomarco@yahoo.com>
2024-11-12 12:04:54 +0100

    finished migration Manga

--- database/migrations/2024_11_12_105137_create_mangas_table.php
+++ database/migrations/2024_11_12_105137_create_mangas_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('mangas', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(false);
+            $table->decimal('price', total: 4, places: 2)->unsigned()->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->string('autor')->nullable(false);
+            $table->string('genre')->nullable(false);
             $table->timestamps();
         });
     }

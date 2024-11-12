2eaeabbbbae39217a733f6d5cd8bca27a92cc2d7
GiorgioDordo <djole6789@gmail.com>
2024-11-12 13:08:48 +0100

    web.php fix

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\MangaController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MangaController;
+
 
 /*
 |--------------------------------------------------------------------------
